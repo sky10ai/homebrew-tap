0d8de807ec059f2a4c59a21f8ec963a4a6edba14
The Bot Father <bf@sky10.ai>
2026-03-18 00:41:50 -0500

    Bump to v0.7.2

--- Formula/sky10-cirrus.rb
+++ Formula/sky10-cirrus.rb
@@ -1,7 +1,7 @@
 class Sky10Cirrus < Formula
   desc "Encrypted file sync for macOS"
   homepage "https://github.com/sky10ai/sky10"
-  url "https://github.com/sky10ai/sky10.git", tag: "v0.7.1"
+  url "https://github.com/sky10ai/sky10.git", tag: "v0.7.2"
   license "MIT"
 
   depends_on "sky10ai/tap/sky10"
--- Formula/sky10.rb
+++ Formula/sky10.rb
@@ -1,13 +1,13 @@
 class Sky10 < Formula
   desc "Encrypted storage & agent coordination"
   homepage "https://github.com/sky10ai/sky10"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sky10ai/sky10/releases/download/v0.7.1/sky10-darwin-arm64"
-      sha256 "11a2e45c91138c557432535f01da5999c9c1d3f50052a79f436f218a108eb63a"
+      url "https://github.com/sky10ai/sky10/releases/download/v0.7.2/sky10-darwin-arm64"
+      sha256 "d68d6caa4a9e012123a455c5fa8e70c16a10aab875e9026022abf29fd6b533e8"
     else
       url "https://github.com/sky10ai/sky10/releases/download/v0.4.2/sky10-darwin-amd64"
       sha256 "af2c1f7a71150263828f9feec6dcc88ac4f147863ab66bb50239dbac3a9aa1b9"
@@ -17,10 +17,10 @@ class Sky10 < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/sky10ai/sky10/releases/download/v0.4.2/sky10-linux-arm64"
-      sha256 "887cf93dc4a97c4c3e1c317db3540.7.1b4ccc9c15362b84e68311d2a32b328e"
+      sha256 "887cf93dc4a97c4c3e1c317db3540.7.2b4ccc9c15362b84e68311d2a32b328e"
     else
       url "https://github.com/sky10ai/sky10/releases/download/v0.4.2/sky10-linux-amd64"
-      sha256 "4bd23af461b2f27d34e44325d0b419a45c20f0.7.17757e495389c5394114fe9"
+      sha256 "4bd23af461b2f27d34e44325d0b419a45c20f0.7.27757e495389c5394114fe9"
     end
   end
 
