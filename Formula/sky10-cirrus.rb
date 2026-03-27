90ea3d99583bf67eed5cf1cf0e7d3d8e1ebf1505
The Bot Father <bf@sky10.ai>
2026-03-26 23:04:31 -0500

    Bump to v0.19.0

--- Formula/sky10-cirrus.rb
+++ Formula/sky10-cirrus.rb
@@ -1,7 +1,7 @@
 class Sky10Cirrus < Formula
   desc "Encrypted file sync for macOS"
   homepage "https://github.com/sky10ai/sky10"
-  url "https://github.com/sky10ai/sky10.git", tag: "v0.18.1"
+  url "https://github.com/sky10ai/sky10.git", tag: "v0.19.0"
   license "Apache-2.0"
 
   depends_on "sky10ai/tap/sky10"
--- Formula/sky10.rb
+++ Formula/sky10.rb
@@ -1,13 +1,13 @@
 class Sky10 < Formula
   desc "Encrypted storage & agent coordination"
   homepage "https://github.com/sky10ai/sky10"
-  version "0.18.1"
+  version "0.19.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sky10ai/sky10/releases/download/v0.18.1/sky10-darwin-arm64"
-      sha256 "f74568ac5a8ea7cab7a23bb4e8f4ace1a7b7cf4dd7660092ef4cea8dd885ef6a"
+      url "https://github.com/sky10ai/sky10/releases/download/v0.19.0/sky10-darwin-arm64"
+      sha256 "53c231c310ddfd2e75f644b76d748e9599b02363db793f2eaa0affab76ebbb3b"
     else
       url "https://github.com/sky10ai/sky10/releases/download/v0.4.2/sky10-darwin-amd64"
       sha256 "af2c1f7a71150263828f9feec6dcc88ac4f147863ab66bb50239dbac3a9aa1b9"
