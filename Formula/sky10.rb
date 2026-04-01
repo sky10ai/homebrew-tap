0aeb2dc65258562ee593d597c3c4590f89d8e10c
The Bot Father <bf@sky10.ai>
2026-03-31 21:38:52 -0500

    Bump to v0.26.1

--- Formula/sky10.rb
+++ Formula/sky10.rb
@@ -1,13 +1,13 @@
 class Sky10 < Formula
   desc "Encrypted storage & agent coordination"
   homepage "https://github.com/sky10ai/sky10"
-  version "0.26.0"
+  version "0.26.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sky10ai/sky10/releases/download/v0.26.0/sky10-darwin-arm64"
-      sha256 "e3ee4a4eafdd915b926f721ab1c38b84ef9643ee69b2eccfe82c763854d2c411"
+      url "https://github.com/sky10ai/sky10/releases/download/v0.26.1/sky10-darwin-arm64"
+      sha256 "4e70c5609f7a3f174fce99e993860e81cdbe263e359d0e8ce9812f8d4d887728"
     end
   end
 
