3c011f3f6e1b38ace1e6f97b20caa33612d991d1
The Bot Father <bf@sky10.ai>
2026-04-03 22:40:54 -0500

    Bump to v0.31.0

--- Formula/sky10.rb
+++ Formula/sky10.rb
@@ -1,13 +1,26 @@
 class Sky10 < Formula
   desc "Encrypted storage & agent coordination"
   homepage "https://github.com/sky10ai/sky10"
-  version "0.30.3"
+  version "0.31.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sky10ai/sky10/releases/download/v0.30.3/sky10-darwin-arm64"
-      sha256 "da9585f1ec1c3c521c4d0882d684c58661f357eaf26e91727b036a0a78f6ade7"
+      url "https://github.com/sky10ai/sky10/releases/download/v0.31.0/sky10-darwin-arm64"
+      sha256 "211a9d8d1a6b734ec4e45a90c2067885a76a9a7ce531b43788f4449684de9cdf"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/sky10ai/sky10/releases/download/v0.31.0/sky10-darwin-amd64"
+      sha256 "0e0f98a9f355c48c2053fe2e59fef123202ffb8b6cda7d36699f883c58df09b6"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/sky10ai/sky10/releases/download/v0.31.0/sky10-linux-arm64"
+      sha256 "dcf899a6a83168061d1829be194c1d52a7c82c902b33ed599150e7e7b3324e51"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/sky10ai/sky10/releases/download/v0.31.0/sky10-linux-amd64"
+      sha256 "a8473df72357b79d8a1132086d0d345afa65bc8177cae8bbef25b9ebcda47cb5"
     end
   end
 
